54d1b3f35f4deb264f24c175b90f2715de473327
ttainam <tainamgbn@gmail.com>
2023-08-14 17:27:16 -0300

    Create config.py
    Criado arquivo config

---
+++ config.py
@@ -0,0 +1,15 @@
+
+# Configurações do PostgreSQL
+PG_CONFIG = {
+    'dbname': 'dvdrental',
+    'user': 'postgres',
+    'password': 'th32s71409',
+    'host': 'localhost'
+}
+
+# Configurações do MongoDB
+MONGO_CONFIG = {
+    'host': 'localhost',
+    'port': 27017,
+    'dbname': 'mydatabase'
+}
