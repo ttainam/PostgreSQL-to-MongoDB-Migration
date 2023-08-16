7b871dfffb963a74e620baf4fb6259eac137499e
ttainam <tainamgbn@gmail.com>
2023-08-15 23:52:58 -0300

    Update utils.py
    adicionado funcao update_collection ao utils

--- utils.py
+++ utils.py
@@ -9,3 +9,17 @@ def convert_value(value):
         return value.isoformat()
     elif isinstance(value, memoryview):
         return bytes(value)
+
+
+def update_collection(collection, filter_document, update_document):
+  """Updates a collection with the given filter document and update document.
+
+  Args:
+    collection: The collection to update.
+    filter_document: The document to filter the collection by.
+    update_document: The document to update the collection with.
+
+  Returns:
+    The number of documents updated.
+  """
+  return collection.update_one(filter_document, update_document)
