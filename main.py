33236ff3a85783a1c1edaeab0af657f7e45d28cd
ttainam <tainamgbn@gmail.com>
2023-09-04 14:20:15 -0300

    Update main.py
    Versão Final

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import psycopg2
-from bson import BSON
 from bson.objectid import ObjectId
 from pymongo import MongoClient
 import traceback
 from config import PG_CONFIG, MONGO_CONFIG
-from utils import convert_value, busca_todas_tabelas_postgress, busca_estrutura_tabela, busca_quantidades_referencias, verifica_campo_pk
+from utils import convert_value, busca_todas_tabelas_postgress, busca_estrutura_tabela, busca_quantidades_referencias, verifica_campo_pk, busca_campo_pk
 
 # Conexão com o banco de dados MongoDB
 mongo_client = MongoClient(MONGO_CONFIG['host'], MONGO_CONFIG['port'])
@@ -15,7 +14,6 @@ pg_connection = psycopg2.connect(**PG_CONFIG)
 
 try:
     tabelas_verificadas = list()
-    tabelas_verificar_mongo = list()
 
     # Buscar tabelas do PostgreSQL
     resultados = busca_todas_tabelas_postgress(pg_connection)
@@ -68,7 +66,9 @@ try:
 
                             column_names = [desc[0] for desc in pg_cursor3.description]
                             if foreign_object:
+                                column_name = fk_info[0]
                                 result_dict = {col_name: col_value for col_name, col_value in zip(column_names, foreign_object)}
+
                                 fk_info_subtable = ''
                                 col_value_name = ''
                                 col_value_subtable = ''
@@ -77,7 +77,9 @@ try:
                                     fk_info_subtable = verifica_campo_pk(pg_connection, fk_info[0], col_name)
 
                                     if fk_info_subtable:
-                                        # Verificar se a subtabela (Tabela C) que 
+                                        result_dict[fk_info_subtable[0]] = result_dict.pop(col_name)
+
+                                        # Verificar se a subtabela (Tabela C) que
                                         # está contida em Tabela B somente tem uma referencia
                                         num_references_keys_referenced_table = busca_quantidades_referencias(pg_connection, fk_info_subtable[0])
 
@@ -87,17 +89,23 @@ try:
                                             values = pg_cursor10.fetchone()
                                             column_names_referenced = [desc[0] for desc in pg_cursor10.description]
                                             col_value_subtable = {col_name: col_value for col_name, col_value in zip(column_names_referenced, values)}
-                                            col_value_name = col_name
+                                            col_value_name = fk_info_subtable[0]
+
+                                            for name, valor in enumerate(column_names_referenced):
+                                                if name > 0:
+                                                    is_pk = verifica_campo_pk(pg_connection, fk_info_subtable[0], valor)
+
+                                                    if is_pk:
+                                                        col_value_subtable[is_pk[0]] = col_value_subtable.pop(valor)
+
                                             if fk_info_subtable[0] not in tabelas_verificadas:
                                                 tabelas_verificadas.append(fk_info_subtable[0])
-                                                print(tabelas_verificadas, fk_info_subtable[0], "abc")
+
                                 if table not in tabelas_verificadas:
                                     tabelas_verificadas.append(table)
-                                    print(tabelas_verificadas, table, "def")
 
                                 if fk_info[0] not in tabelas_verificadas:
                                     tabelas_verificadas.append(fk_info[0])
-                                    print(tabelas_verificadas, table, fk_info[0], "fgh")
 
                                 for key, valor in result_dict.items():
                                     if convert_value(valor):
@@ -108,46 +116,57 @@ try:
                                     result_dict[col_value_name] = col_value_subtable
                             else:
                                 document[column_name] = value
-
                         else:
-                            document[column_name] = value
-                            if table not in tabelas_verificar_mongo:
-                                tabelas_verificar_mongo.append(table)
-                    elif fk_info and fk_info[0] != table:
-                        document[column_name] = value
-                        if table not in tabelas_verificar_mongo:
-                            tabelas_verificar_mongo.append(table)
+                            document[fk_info[0]] = value
                     else:
                         if convert_value(value):
                             document[column_name] = convert_value(value)
                         else:
                             document[column_name] = value
             mongo_collection.insert_one(document)
-            # print(document)
 
-    # for table in tabelas_verificar_mongo:
-    #     # Buscar estrutura da tabela
-    #     pg_cursor6 = pg_connection.cursor()
-    #     pg_cursor6.execute(f"SELECT column_name, data_type FROM information_schema.columns WHERE table_name='{table}';")
-    #     columns = {row[0]: row[1] for row in pg_cursor6.fetchall()}
-    #
-    #     pg_cursor7 = pg_connection.cursor()
-    #     pg_cursor7.execute(f"SELECT * FROM {table};")
-    #     data = pg_cursor7.fetchall()
-    #     for row in data:
-    #
-    #         for i, value in enumerate(row):
-    #             column_name = pg_cursor.description[i].name
-    #             pg_cursor5 = pg_connection.cursor()
-    #             pg_cursor5.execute(
-    #                 f"SELECT ccu.table_name AS referenced_table, ccu.column_name AS referenced_column FROM information_schema.key_column_usage kcu JOIN information_schema.constraint_column_usage ccu ON kcu.constraint_name = ccu.constraint_name WHERE kcu.table_name = '{table}' AND kcu.column_name = '{column_name}'")
-    #             fk_info = pg_cursor5.fetchone()
-    #             if fk_info and fk_info[0] != table:
-    #                 referenced_collection = mongo_db[fk_info[0]]
-    #                 matching_document = referenced_collection.find_one({fk_info[1]: value})
-    #
-    #                 if matching_document:
-    #                     mongo_db[table].update_one(matching_document,  {"$set": {fk_info[1]: ObjectId(matching_document['_id'])}})
+    collection_names = mongo_db.list_collection_names()
+
+    # Imprimir o conteúdo de todos os documentos em cada coleção
+    for collection_name in collection_names:
+        collection = mongo_db[collection_name]
+        documents = collection.find()
+        for document in documents:
+            for collumn, value in document.items():
+                update_dict = {}
+                if isinstance(value, int):
+                    coluna_pk = busca_campo_pk(pg_connection, collumn)
+                    if coluna_pk is not None:
+                        fk_info = verifica_campo_pk(pg_connection, collection_name, coluna_pk[0])
+                        if fk_info and fk_info[0] != collection_name:
+                            referenced_collection = mongo_db[fk_info[0]]
+                            matching_document = referenced_collection.find_one({fk_info[1]: value})
+                            if matching_document:
+                                mongo_db[collection_name].update_one(document,  {"$set": {collumn: ObjectId(matching_document['_id'])}})
+                if isinstance(value, dict):
+                    for key, val in value.items():
+                        if isinstance(val, int):
+                            coluna_pk = busca_campo_pk(pg_connection, key)
+                            if coluna_pk is not None:
+                                fk_info = verifica_campo_pk(pg_connection, collection_name, coluna_pk[0])
+                                if fk_info and fk_info[0] != collection_name:
+                                    referenced_collection = mongo_db[fk_info[0]]
+                                    matching_document = referenced_collection.find_one({fk_info[1]: val})
+                                    if matching_document:
+                                        update_dict[f"{collumn}.{key}"] = ObjectId(matching_document['_id'])
+                        if isinstance(val, dict) and collumn != key:
+                            for k, v in val.items():
+                                if isinstance(v, int):
+                                    coluna_pk = busca_campo_pk(pg_connection, k)
+                                    if coluna_pk is not None:
+                                        fk_info = verifica_campo_pk(pg_connection, key, coluna_pk[0])
+                                        if fk_info and fk_info[0] != key:
+                                            referenced_collection = mongo_db[fk_info[0]]
+                                            matching_document = referenced_collection.find_one({fk_info[1]: v})
+                                            if matching_document:
+                                                update_dict[f"{collumn}.{key}.{k}"] = ObjectId(matching_document['_id'])
+                if update_dict:
+                    mongo_db[collection_name].update_one({"_id": document["_id"]}, {"$set": update_dict})
 
 except Exception as e:
     print("Erro:", e)
